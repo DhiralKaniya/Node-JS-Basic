fc26dd736a88eddd3f4c52a8107c71d3a02fff6a
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-05-02 13:42:59 +0530

    Array Implementation in Node

--- array.js
+++ array.js
@@ -7,5 +7,5 @@ arr.push(17);
 var value = 1;
 arr.forEach(function(item) {
     if(item==value)
-        console.log('founded' + item);
+        console.log('Your value has been founded' + item);
 });
