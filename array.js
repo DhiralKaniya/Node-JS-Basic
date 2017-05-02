9b43c7fd2ffb3584c73ea9b61704651bef6df6c0
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-05-02 13:40:12 +0530

    Array Implementation in java

---
+++ array.js
@@ -0,0 +1,11 @@
+var arr = []
+arr.push(20);
+arr.push(10);
+arr.push(15);
+arr.push(9);
+arr.push(17);
+var value = 1;
+arr.forEach(function(item) {
+    if(item==value)
+        console.log('founded' + item);
+});
